8214968fa2f25fcf1659176f22bf0ccb6798530f
Farzaneh Seyedshahi <44030693+FarzanehSeyedshahi@users.noreply.github.com>
2022-08-15 13:39:32 +0200

    Adding My Synthetic Dataset

--- train.py
+++ train.py
@@ -5,7 +5,6 @@ import argparse
 from keras.callbacks import ModelCheckpoint, TensorBoard
 from keras.models import load_model
 from tensorflow.keras.optimizers import Adam
-
 from sklearn.utils import shuffle
 
 from time import time
@@ -16,7 +15,7 @@ from util import *
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('dataset', choices=['oxiod', 'euroc'], help='Training dataset name (\'oxiod\' or \'euroc\')')
+    parser.add_argument('dataset', choices=['oxiod', 'euroc', 'synthetic'], help='Training dataset name (\'oxiod\' or \'euroc\')')
     parser.add_argument('output', help='Model output name')
     args = parser.parse_args()
 
@@ -34,67 +33,73 @@ def main():
     imu_data_filenames = []
     gt_data_filenames = []
     
-    if args.dataset == 'Synthetic':
-        imu_data_filenames.append()
-        gt_data_filenames.append()
-        
-    if args.dataset == 'oxiod':
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data5/syn/imu3.csv')
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data2/syn/imu1.csv')
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data2/syn/imu2.csv')
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data5/syn/imu2.csv')
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data3/syn/imu4.csv')
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data4/syn/imu4.csv')
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data4/syn/imu2.csv')
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data1/syn/imu7.csv')
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data5/syn/imu4.csv')
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data4/syn/imu5.csv')
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data1/syn/imu3.csv')
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data3/syn/imu2.csv')
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data2/syn/imu3.csv')
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data1/syn/imu1.csv')
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data3/syn/imu3.csv')
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data3/syn/imu5.csv')
-        imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data1/syn/imu4.csv')
+    if args.dataset == 'synthetic':
+        cur_gyro_data, cur_acc_data, cur_pos_data, cur_ori_data = load_synthetic_dataset("dataset")
+        [cur_x_gyro, cur_x_acc], [cur_y_delta_p, cur_y_delta_q], init_p, init_q = load_dataset_6d_quat(cur_gyro_data, cur_acc_data, cur_pos_data, cur_ori_data, window_size, stride)
 
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data5/syn/vi3.csv')
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data2/syn/vi1.csv')
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data2/syn/vi2.csv')
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data5/syn/vi2.csv')
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data3/syn/vi4.csv')
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data4/syn/vi4.csv')
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data4/syn/vi2.csv')
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data1/syn/vi7.csv')
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data5/syn/vi4.csv')
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data4/syn/vi5.csv')
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data1/syn/vi3.csv')
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data3/syn/vi2.csv')
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data2/syn/vi3.csv')
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data1/syn/vi1.csv')
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data3/syn/vi3.csv')
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data3/syn/vi5.csv')
-        gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data1/syn/vi4.csv')
-    
-    elif args.dataset == 'euroc':
-        imu_data_filenames.append('MH_01_easy/mav0/imu0/data.csv')
-        imu_data_filenames.append('MH_03_medium/mav0/imu0/data.csv')
-        imu_data_filenames.append('MH_05_difficult/mav0/imu0/data.csv')
-        imu_data_filenames.append('V1_02_medium/mav0/imu0/data.csv')
-        imu_data_filenames.append('V2_01_easy/mav0/imu0/data.csv')
-        imu_data_filenames.append('V2_03_difficult/mav0/imu0/data.csv')
+        x_gyro.append(cur_x_gyro)
+        x_acc.append(cur_x_acc)
 
-        gt_data_filenames.append('MH_01_easy/mav0/state_groundtruth_estimate0/data.csv')
-        gt_data_filenames.append('MH_03_medium/mav0/state_groundtruth_estimate0/data.csv')
-        gt_data_filenames.append('MH_05_difficult/mav0/state_groundtruth_estimate0/data.csv')
-        gt_data_filenames.append('V1_02_medium/mav0/state_groundtruth_estimate0/data.csv')
-        gt_data_filenames.append('V2_01_easy/mav0/state_groundtruth_estimate0/data.csv')
-        gt_data_filenames.append('V2_03_difficult/mav0/state_groundtruth_estimate0/data.csv')
-
-    for i, (cur_imu_data_filename, cur_gt_data_filename) in enumerate(zip(imu_data_filenames, gt_data_filenames)):
+        y_delta_p.append(cur_y_delta_p)
+        y_delta_q.append(cur_y_delta_q)
+    else:
         if args.dataset == 'oxiod':
-            cur_gyro_data, cur_acc_data, cur_pos_data, cur_ori_data = load_oxiod_dataset(cur_imu_data_filename, cur_gt_data_filename)
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data5/syn/imu3.csv')
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data2/syn/imu1.csv')
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data2/syn/imu2.csv')
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data5/syn/imu2.csv')
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data3/syn/imu4.csv')
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data4/syn/imu4.csv')
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data4/syn/imu2.csv')
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data1/syn/imu7.csv')
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data5/syn/imu4.csv')
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data4/syn/imu5.csv')
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data1/syn/imu3.csv')
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data3/syn/imu2.csv')
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data2/syn/imu3.csv')
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data1/syn/imu1.csv')
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data3/syn/imu3.csv')
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data3/syn/imu5.csv')
+            imu_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data1/syn/imu4.csv')
+
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data5/syn/vi3.csv')
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data2/syn/vi1.csv')
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data2/syn/vi2.csv')
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data5/syn/vi2.csv')
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data3/syn/vi4.csv')
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data4/syn/vi4.csv')
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data4/syn/vi2.csv')
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data1/syn/vi7.csv')
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data5/syn/vi4.csv')
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data4/syn/vi5.csv')
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data1/syn/vi3.csv')
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data3/syn/vi2.csv')
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data2/syn/vi3.csv')
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data1/syn/vi1.csv')
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data3/syn/vi3.csv')
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data3/syn/vi5.csv')
+            gt_data_filenames.append('Oxford Inertial Odometry Dataset/handheld/data1/syn/vi4.csv')
+        
         elif args.dataset == 'euroc':
-            cur_gyro_data, cur_acc_data, cur_pos_data, cur_ori_data = load_euroc_mav_dataset(cur_imu_data_filename, cur_gt_data_filename)
+            imu_data_filenames.append('MH_01_easy/mav0/imu0/data.csv')
+            imu_data_filenames.append('MH_03_medium/mav0/imu0/data.csv')
+            imu_data_filenames.append('MH_05_difficult/mav0/imu0/data.csv')
+            imu_data_filenames.append('V1_02_medium/mav0/imu0/data.csv')
+            imu_data_filenames.append('V2_01_easy/mav0/imu0/data.csv')
+            imu_data_filenames.append('V2_03_difficult/mav0/imu0/data.csv')
+
+            gt_data_filenames.append('MH_01_easy/mav0/state_groundtruth_estimate0/data.csv')
+            gt_data_filenames.append('MH_03_medium/mav0/state_groundtruth_estimate0/data.csv')
+            gt_data_filenames.append('MH_05_difficult/mav0/state_groundtruth_estimate0/data.csv')
+            gt_data_filenames.append('V1_02_medium/mav0/state_groundtruth_estimate0/data.csv')
+            gt_data_filenames.append('V2_01_easy/mav0/state_groundtruth_estimate0/data.csv')
+            gt_data_filenames.append('V2_03_difficult/mav0/state_groundtruth_estimate0/data.csv')        
+
+        for i, (cur_imu_data_filename, cur_gt_data_filename) in enumerate(zip(imu_data_filenames, gt_data_filenames)):
+            if args.dataset == 'oxiod':
+                cur_gyro_data, cur_acc_data, cur_pos_data, cur_ori_data = load_oxiod_dataset(cur_imu_data_filename, cur_gt_data_filename)
+            elif args.dataset == 'euroc':
+                cur_gyro_data, cur_acc_data, cur_pos_data, cur_ori_data = load_euroc_mav_dataset(cur_imu_data_filename, cur_gt_data_filename)
 
         [cur_x_gyro, cur_x_acc], [cur_y_delta_p, cur_y_delta_q], init_p, init_q = load_dataset_6d_quat(cur_gyro_data, cur_acc_data, cur_pos_data, cur_ori_data, window_size, stride)
 
@@ -104,6 +109,7 @@ def main():
         y_delta_p.append(cur_y_delta_p)
         y_delta_q.append(cur_y_delta_q)
 
+    
     x_gyro = np.vstack(x_gyro)
     x_acc = np.vstack(x_acc)
 
