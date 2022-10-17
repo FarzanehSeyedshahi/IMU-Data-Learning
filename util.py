db96685e3c4e76fa7d0a15ca9411b10a5aed5560
Farzaneh Seyedshahi <44030693+FarzanehSeyedshahi@users.noreply.github.com>
2022-10-17 12:14:06 +0200

    Update util.py (last version before dataset)
    - Adding transformations

--- util.py
+++ util.py
@@ -1,22 +1,21 @@
 import numpy as np
 import quaternion
-
+import tensorflow as tf
+from tqdm import tqdm
+import pywt
 
 def generate_trajectory_6d_quat(init_p, init_q, y_delta_p, y_delta_q):
     cur_p = np.array(init_p)
     cur_q = quaternion.from_float_array(init_q)
     pred_p = []
-    pred_q = []
     pred_p.append(np.array(cur_p))
-    pred_q.append(np.array(init_q))
 
     for [delta_p, delta_q] in zip(y_delta_p, y_delta_q):
         cur_p = cur_p + np.matmul(quaternion.as_rotation_matrix(cur_q), delta_p.T).T
         cur_q = cur_q * quaternion.from_float_array(delta_q).normalized()
         pred_p.append(np.array(cur_p))
-        pred_q.append(np.asarray(cur_q, dtype=np.quaternion).view((np.double, 4)))
-        
-    return np.reshape(pred_p, (len(pred_p), 3)), np.reshape(pred_q, (len(pred_q),4))
+
+    return np.reshape(pred_p, (len(pred_p), 3))
 
 
 def generate_trajectory_3d(init_l, init_theta, init_psi, y_delta_l, y_delta_theta, y_delta_psi):
@@ -35,3 +34,48 @@ def generate_trajectory_3d(init_l, init_theta, init_psi, y_delta_l, y_delta_thet
         pred_l.append(np.array(cur_l))
 
     return np.reshape(pred_l, (len(pred_l), 3))
+
+def causal_mask(batch, size):
+    x, y = tf.expand_dims(tf.range(size),1), tf.range(size)
+    mask = x>=y
+    mask = tf.reshape(mask, (1, size, size))
+    mask = tf.tile(mask, (batch, 1, 1))
+    return mask
+
+def create_cwt_images(X, scale_range, wavelet_name = 'morl', rescale=True, upsample=False, rescale_steps=30, rescale_scales=30):
+    samples = X.shape[0]
+    steps = X.shape[1] 
+    sensors = X.shape[2] 
+
+    x_dim = steps
+    y_dim = len(scale_range)
+    
+    if upsample:
+        x_dim = x_dim * 2
+
+    if rescale:
+        x_dim = rescale_steps
+        y_dim = rescale_scales
+    
+    
+    # prepare the output array
+    X_cwt = np.ndarray(shape=(samples, y_dim, x_dim, sensors), dtype = 'float32')
+    
+    for sample in tqdm(range(samples),desc = 'Creating CWT transformed vectors: '):
+        
+        for sensor in range(sensors):
+            series = X[sample, :, sensor]
+            # upsample
+            if upsample:
+                x = np.linspace(1,len(series),len(series)*2)
+                xp = np.arange(1,len(series)+1,dtype='float32')
+                series = np.interp(x, xp, series)
+            # continuous wavelet transform 
+            coeffs, _ = pywt.cwt(series, scale_range, wavelet_name)
+            # resize the 2D cwt coeffs
+            if rescale:
+                coeffs = resize(coeffs, (y_dim, x_dim), mode = 'constant')
+
+            X_cwt[sample,:,:,sensor] = coeffs
+            
+    return X_cwt
