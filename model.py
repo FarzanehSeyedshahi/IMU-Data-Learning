732da8e70b469eb3d945131159884db11f8eef90
Farzaneh Seyedshahi <44030693+FarzanehSeyedshahi@users.noreply.github.com>
2022-10-17 12:18:25 +0200

    Update model.py (last version before dataset)
    - Adding some new architectures

--- model.py
+++ model.py
@@ -1,8 +1,12 @@
+from turtle import position
 import tfquaternion as tfq
 import tensorflow as tf
+from util import *
+import tensorflow_addons as tfa
+
 
 from keras.models import Sequential, Model
-from keras.layers import Bidirectional, CuDNNLSTM, Dropout, Dense, Input, Layer, Conv1D, MaxPooling1D, concatenate, Add, Activation
+from keras.layers import Bidirectional, Flatten, Reshape, Lambda, CuDNNLSTM, Dropout, Dense, Input, MaxPooling2D, Layer, Conv2D, Conv1D, MaxPooling1D, concatenate, Add, Activation
 from keras.initializers import Constant
 from tensorflow.keras.optimizers import Adam
 from keras.losses import mean_absolute_error
@@ -11,7 +15,7 @@ from keras import backend as K
 from tensorflow import keras
 from tensorflow.keras import layers
 
-window_size_transformer = 600
+window_size_transformer = 200
 
 def quaternion_phi_3_error(y_true, y_pred):
     return tf.acos(K.abs(K.batch_dot(y_true, K.l2_normalize(y_pred, axis=-1), axes=-1)))
@@ -328,10 +332,107 @@ def create_train_resnet_or_without_model_9d_quat(pred_model, window_size):
     y2_true = Input(shape=(4,), name='y2_true')
     out = CustomMultiLossLayer(nb_outputs=2)([y1_true, y2_true, y1_pred, y2_pred])
     train_model = Model([x1, x2, x3, y1_true, y2_true], out)
-    train_model.summary()
+    # train_model.summary()
     return train_model
 
 #################################### Transformer models#####################################
+# class PositionalEncoding(torch.nn.Module):
+
+#     def __init__(self, d_model, dropout=0.1, max_len=5000):
+#         self.dropout = torch.nn.Dropout(p=dropout)
+
+#         pe = torch.zeros(max_len, (d_model + 1) // 2 * 2)
+#         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+#         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+#         pe[:, 0::2] = torch.sin(position * div_term)
+#         pe[:, 1::2] = torch.cos(position * div_term)
+#         pe = pe.unsqueeze(0).transpose(0, 1)
+#         self.register_buffer('pe', pe)
+
+#     def forward(self, x):
+#         r"""Inputs of forward function
+#         Args:
+#             x: the sequence fed to the positional encoder model (required).
+#         Shape:
+#             x: [sequence length, batch size, embed dim]
+#             output: [sequence length, batch size, embed dim]
+#         Examples:
+#         """
+
+#         x = x + self.pe[:x.size(0), :, :x.size(2)]
+#         return self.dropout(x)
+
+class PositionEncoding(layers.Layer):
+    def __init__(self, maxlength, window_size, embed_dim, **kwargs):
+        super(PositionEncoding, self).__init__()
+        self.window_embed = layers.Embedding(window_size, embed_dim)
+
+        pe = tf.zeros(maxlength, (embed_dim + 1) // 2 * 2)
+        position = tf.range(0, maxlength)
+        div_term = tf.math.exp(tf.range(0, embed_dim, 2).float() * (-tf.math.log(10000.0) / embed_dim))
+        pe[:, 0::2] = tf.math.sin(position * div_term)
+        pe[:, 1::2] = tf.math.cos(position * div_term)
+        self.position_encode = pe
+        super(PositionEncoding, self).__init__(**kwargs)
+
+    def call(self, x):
+        x = self.window_embed(x)
+        return x + self.position_encode
+
+
+
+
+class PositionEmbedding(layers.Layer):
+    def __init__(self, maxlength, window_size, embed_dim, **kwargs):
+        super(PositionEmbedding, self).__init__()
+        self.window_embed = layers.Embedding(window_size, embed_dim)
+        self.position_embed = layers.Embedding(maxlength, embed_dim)
+        super(PositionEmbedding, self).__init__(**kwargs)
+
+    def call(self, x):
+        maxlength = tf.shape(x)[-1]
+        positions = tf.range(0, maxlength)
+
+        positions = self.position_embed(positions)
+        x = self.window_embed(x)
+        return x + positions
+
+
+
+class TransformerBlock(layers.Layer):
+    def __init__(self, embed_dims, num_heads, dropout, **kwargs):
+        super(TransformerBlock, self).__init__()
+        self.attention_layer =  layers.MultiHeadAttention(num_heads, embed_dims)
+        self.dropout_layer_1 = layers.Dropout(dropout)
+        self.norm_layer_1 = layers.LayerNormalization(epsilon=1e-6)
+        self.feed_forward_network_layers = Sequential([
+            layers.Dense(embed_dims, activation='relu'),
+            layers.Dense(embed_dims)
+        ])
+        self.dropout_layer_2 = layers.Dropout(dropout)
+        self.norm_layer_2 = layers.LayerNormalization(epsilon=1e-6)
+        super(TransformerBlock, self).__init__(**kwargs)
+
+    def call(self, inputs):
+        # batch_size, sequence_length, embed_dime = tf.shape(inputs)[0], tf.shape(inputs)[1], tf.shape(inputs)[2]
+        #Causal Masking
+        # mask = causal_mask(batch_size, sequence_length, embed_dime)
+        # mask = self.
+        # Multiheaded Attention
+        mha_out = self.attention_layer(inputs, inputs)
+        dropout_1_out = self.dropout_layer_1(mha_out)
+        norm_1_out = self.norm_layer_1(dropout_1_out)
+        combined_1_out = norm_1_out + inputs
+        #feedforward network
+        ffn_out = self.feed_forward_network_layers(combined_1_out)
+        dropout_2_out = self.dropout_layer_2(ffn_out)
+        norm_2_out = self.norm_layer_2(dropout_2_out)
+        combined_2_out = norm_2_out + combined_1_out
+
+        return combined_2_out
+
+
+
 
 def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout):
     #Normalization and attention
@@ -345,7 +446,6 @@ def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout):
     x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(x)
     x = layers.Dropout(dropout)(x)
     x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
-
     return x+res
 
 def create_transformer_pred_model_9d(
@@ -362,6 +462,10 @@ def create_transformer_pred_model_9d(
     x1 = Input((window_size, 3), name='x1')
     x2 = Input((window_size, 3), name='x2')
     x3 = Input((window_size, 3), name='x3')
+
+    x1 = layers.BatchNormalization(trainable=True, epsilon=1e-9)(x1)
+    x2 = layers.BatchNormalization(trainable=True, epsilon=1e-9)(x2)
+    x3 = layers.BatchNormalization(trainable=True, epsilon=1e-9)(x3)
 
     convA1 = Conv1D(128, 11, activation='relu', padding = "same")(x1)
     conv_shortcut_A1 = convA1
@@ -387,10 +491,15 @@ def create_transformer_pred_model_9d(
     actC = Activation('relu')(addC)
     poolC = MaxPooling1D(3)(actC)
 
-    ABC = concatenate([poolA, poolB, poolC])
+    x = concatenate([poolA, poolB, poolC])
 
-    for _ in range(num_transformer_blocks):
-        x = transformer_encoder(ABC, head_size, num_heads, ff_dim, dropout)
+    #Embedding
+    # x = PositionEmbedding(maxlength=200, window_size=200, embed_dim=32)(x)
+
+    # for _ in range(num_transformer_blocks):
+    x = transformer_encoder(x, 32, num_heads, ff_dim, dropout)
+    x = transformer_encoder(x, 32, num_heads, ff_dim, dropout)
+    x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
 
     x = layers.GlobalAveragePooling1D(data_format="channels_first")(x)
     for dim in mlp_units:
@@ -401,7 +510,320 @@ def create_transformer_pred_model_9d(
 
 
     model = Model([x1, x2, x3], [y1_pred, y2_pred])
-    model.summary()
+    # model.summary()
     # return keras.Model(inputs, outputs)
     return model
     
+
+
+### Adding VIT model
+def mlp(x, hidden_units, dropout_rate):
+    for units in hidden_units:
+        x = layers.Dense(units, activation=tf.nn.gelu)(x)
+        x = layers.Dropout(dropout_rate)(x)
+    return x
+
+
+class Patches(layers.Layer):
+    def __init__(self, patch_size, **kwargs):
+        super(Patches, self).__init__()
+        self.patch_size = patch_size
+        super(Patches, self).__init__(**kwargs)
+
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            "patch_size": self.patch_size,
+        })
+        return config
+
+
+    def call(self, images):
+        batch_size = tf.shape(images)[0]
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size[0], self.patch_size[1], 1],
+            strides=[1, self.patch_size[0], self.patch_size[1], 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        patch_dims = patches.shape[-1]
+        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+        return patches
+
+
+class PatchEncoder(layers.Layer):
+    def __init__(self, num_patches, projection_dim, **kwargs):
+        super(PatchEncoder, self).__init__()
+        self.num_patches = num_patches
+        self.projection = layers.Dense(units=projection_dim)
+        self.position_embedding = layers.Embedding(
+            input_dim=num_patches, output_dim=projection_dim
+        )
+        self.projection_dim = projection_dim
+        super(PatchEncoder, self).__init__(**kwargs)
+    
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            "num_patches": self.num_patches,
+            "projection_dim": self.projection_dim
+        })
+        return config
+
+    #Embeddig
+    def call(self, patch):
+        positions = tf.range(start=0, limit=self.num_patches, delta=1)
+        encoded = self.projection(patch) + self.position_embedding(positions)
+        return encoded
+
+    #Encoding
+    # def call(self, patch):
+    #     print(self.num_patches, self.projection_dim)
+    #     pe_np = np.array((self.num_patches, (self.projection_dim + 1) // 2 * 2))
+    #     # pe = tf.zeros([self.num_patches, (self.projection_dim + 1) // 2 * 2])
+    #     # print(tf.shape(pe))
+    #     position = tf.range(0, self.num_patches, dtype=tf.float32)
+    #     print(tf.shape(position))
+
+    #     div_term = tf.math.exp( tf.range(0, self.num_patches, dtype=tf.float32) * (-tf.math.log(10000.0) / self.projection_dim))
+    #     print(tf.shape(div_term))
+    #     print(tf.math.sin(position * div_term))
+    #     pe_np[:, 0::2] = tf.math.sin(position * div_term)
+    #     pe_np[:, 1::2] = tf.math.cos(position * div_term)
+
+    #     pe = tf.convert_to_tensor(pe_np)
+    #     encoded = self.projection(patch) + pe
+    #     return encoded
+
+
+def create_vit_classifier(input_shape, image_size, patch_size, projection_dim, num_heads):
+
+    num_patches = (image_size[0] // patch_size[0]) * (image_size[1] // patch_size[1])
+
+    transformer_units = [
+        projection_dim * 2,
+        projection_dim,
+    ]  # Size of the transformer layers
+    transformer_layers = 12
+    # mlp_head_units = [2048, 1024]  # Size of the dense layers of the final classifier
+
+    inputs = layers.Input(shape=input_shape)
+    # Create patches.
+    patches = Patches(patch_size)(inputs)
+    # print("patches shape:",tf.shape(patches))
+
+    # Encode patches.
+    encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
+    # print("encoded_patches shape:",tf.shape(encoded_patches))
+
+
+    # Create multiple layers of the Transformer block.
+    for _ in range(transformer_layers):
+        x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+        # Create a multi-head attention layer.
+        attention_output = layers.MultiHeadAttention(
+            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+        )(x1, x1)
+        # Skip connection 1.
+        x2 = layers.Add()([attention_output, encoded_patches])
+        # Layer normalization 2.
+        x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+        # MLP.
+        x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
+        # Skip connection 2.
+        encoded_patches = layers.Add()([x3, x2])
+
+    # Create a [batch_size, projection_dim] tensor.
+    representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+    
+    lstm1 = Bidirectional(CuDNNLSTM(128, return_sequences=True))(representation)
+    drop1 = Dropout(0.25)(lstm1)
+    lstm2 = Bidirectional(CuDNNLSTM(128))(drop1)
+    drop2 = Dropout(0.25)(lstm2)    
+
+    # Classify outputs.
+    y_true1 = layers.Dense(3)(drop2)
+    y_true2 = layers.Dense(4)(drop2)
+
+    # Create the Keras model.
+    model = keras.Model(inputs=inputs, outputs=[y_true1,y_true2])
+    return model
+
+
+def create_VIT_model_9d(pred_model, window_size):
+    # x1 = Input((window_size, 3), name='x1')
+    # x2 = Input((window_size, 3), name='x2')
+    # x3 = Input((window_size, 3), name='x3')
+    x = Input((window_size, 9), name='x')
+    y1_pred, y2_pred = pred_model(x)
+    y1_true = Input(shape=(3,), name='y1_true')
+    y2_true = Input(shape=(4,), name='y2_true')
+    out = CustomMultiLossLayer(nb_outputs=2)([y1_true, y2_true, y1_pred, y2_pred])
+    train_model = Model([x, y1_true, y2_true], out)
+    return train_model
+
+def create_combined_transformer_pred_model_9d(window_size):
+
+    x1 = Input((window_size, 3), name='x1')
+    x2 = Input((window_size, 3), name='x2')
+    x3 = Input((window_size, 3), name='x3')
+
+    x1 = layers.BatchNormalization(trainable=True, epsilon=1e-9)(x1)
+    x2 = layers.BatchNormalization(trainable=True, epsilon=1e-9)(x2)
+    x3 = layers.BatchNormalization(trainable=True, epsilon=1e-9)(x3)
+
+    convA1 = Conv1D(128, 11, activation='relu', padding = "same")(x1)
+    conv_shortcut_A1 = convA1
+    convA2 = Conv1D(128, 11, activation='relu', padding = "same")(convA1)
+    convA3 = Conv1D(128, 11, activation='relu', padding = "same")(convA2)
+    addA = Add()([convA3, conv_shortcut_A1])
+    actA = Activation('relu')(addA)
+    poolA = MaxPooling1D(3)(actA)
+
+    convB1 = Conv1D(128, 11, activation='relu', padding = "same")(x2)
+    conv_shortcut_B1 = convB1
+    convB2 = Conv1D(128, 11, activation='relu', padding = "same")(convB1)
+    convB3 = Conv1D(128, 11, activation='relu', padding = "same")(convB2)
+    addB = Add()([convB3, conv_shortcut_B1])
+    actB = Activation('relu')(addB)
+    poolB = MaxPooling1D(3)(actB)
+
+    convC1 = Conv1D(128, 11, activation='relu', padding = "same")(x3)
+    conv_shortcut_C1 = convC1
+    convC2 = Conv1D(128, 11, activation='relu', padding = "same")(convC1)
+    convC3 = Conv1D(128, 11, activation='relu', padding = "same")(convC2)
+    addC = Add()([convC3, conv_shortcut_C1])
+    actC = Activation('relu')(addC)
+    poolC = MaxPooling1D(3)(actC)
+
+    x = concatenate([poolA, poolB, poolC])
+    x = layers.Reshape((66, 384,1), input_shape=(66, 384))(x)
+
+    image_size = (66, 384)
+    patch_size = (6,32)
+    num_patches = (image_size[0] // patch_size[0]) * (image_size[1] // patch_size[1])
+    projection_dim = 64
+    num_heads = 8
+    transformer_units = [
+        projection_dim * 2,
+        projection_dim,
+    ]  # Size of the transformer layers
+    transformer_layers = 8
+    mlp_head_units = [2048, 1024]  # Size of the dense layers of the final classifier
+
+    patches = Patches(patch_size)(x)
+    # # print("patches shape:",tf.shape(patches))
+
+    # # Encode patches.
+    encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
+    # # print("encoded_patches shape:",tf.shape(encoded_patches))
+
+
+    # Create multiple layers of the Transformer block.
+    for _ in range(transformer_layers):
+        # Layer normalization 1.
+        x_normalization = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+        # Create a multi-head attention layer.
+        attention_output = layers.MultiHeadAttention(
+            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+        )(x_normalization, x_normalization)
+        # Skip connection 1.
+        Adding_output = layers.Add()([attention_output, encoded_patches])
+        # Layer normalization 2.
+        x2_normalization = layers.LayerNormalization(epsilon=1e-6)(Adding_output)
+        # MLP.
+        x_mlp = mlp(x2_normalization, hidden_units=transformer_units, dropout_rate=0.1)
+        # Skip connection 2.
+        encoded_patches = layers.Add()([x_mlp, Adding_output])
+
+    # # Create a [batch_size, projection_dim] tensor.
+    representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+    # # print("representation shape:",tf.shape(representation))
+
+    representation = layers.Flatten()(representation)
+    # # print("representation shape:",tf.shape(representation))
+
+    representation = layers.Dropout(0.5)(representation)
+    # # Add MLP.
+    features = mlp(representation, hidden_units=mlp_head_units, dropout_rate=0.5)
+    # # print("features shape:",tf.shape(features))
+
+    # # Classify outputs.
+    y1_pred = layers.Dense(3)(features)
+    y2_pred = layers.Dense(4)(features)
+
+    # # Create the Keras model.
+    model = keras.Model([x1, x2, x3], [y1_pred, y2_pred])
+    return model
+
+################################# Adding CWT ######################
+
+def ReshapeLayer(x):
+    
+    shape = x.shape
+    
+    # 1 possibility: H,W*channel
+    reshape = Reshape((shape[1],shape[2]*shape[3]))(x)
+    
+    # 2 possibility: W,H*channel
+    # transpose = Permute((2,1,3))(x)
+    # reshape = Reshape((shape[1],shape[2]*shape[3]))(transpose)
+    
+    return reshape
+
+def create_pred_resnet_cwt(sample_size, window_size=200):
+    
+    x1 = Input((sample_size, window_size, 3), name='x1')
+    x2 = Input((sample_size, window_size, 3), name='x2')
+    x3 = Input((sample_size, window_size, 3), name='x3')
+
+    convA1 = Conv2D(128, 11, activation='relu', padding = "same")(x1)
+    conv_shortcut_A1 = convA1
+    convA2 = Conv2D(128, 11, activation='relu', padding = "same")(convA1)
+    convA3 = Conv2D(128, 11, activation='relu', padding = "same")(convA2)
+    addA = Add()([convA3, conv_shortcut_A1])
+    actA = Activation('relu')(addA)
+    poolA = MaxPooling2D((3,3))(actA)
+
+    convB1 = Conv2D(128, 11, activation='relu', padding = "same")(x2)
+    conv_shortcut_B1 = convB1
+    convB2 = Conv2D(128, 11, activation='relu', padding = "same")(convB1)
+    convB3 = Conv2D(128, 11, activation='relu', padding = "same")(convB2)
+    addB = Add()([convB3, conv_shortcut_B1])
+    actB = Activation('relu')(addB)
+    poolB = MaxPooling2D((3,3))(actB)
+
+    convC1 = Conv2D(128, 11, activation='relu', padding = "same")(x3)
+    conv_shortcut_C1 = convC1
+    convC2 = Conv2D(128, 11, activation='relu', padding = "same")(convC1)
+    convC3 = Conv2D(128, 11, activation='relu', padding = "same")(convC2)
+    addC = Add()([convC3, conv_shortcut_C1])
+    actC = Activation('relu')(addC)
+    poolC = MaxPooling2D((3,3))(actC)
+
+    ABC = concatenate([poolA, poolB, poolC])
+
+    ABC = Lambda(ReshapeLayer)(ABC) # <========== pass from 4D to 3D
+    ABC = Flatten()(ABC)
+    # lstm1 = Bidirectional(CuDNNLSTM(128, return_sequences=True))(ABC)
+    # drop1 = Dropout(0.25)(lstm1)
+    # lstm2 = Bidirectional(CuDNNLSTM(128))(drop1)
+    # drop2 = Dropout(0.25)(ABC)    
+    y1_pred = Dense(3)(ABC)
+    y2_pred = Dense(4)(ABC)
+
+    model = Model([x1, x2, x3], [y1_pred, y2_pred])
+    return model
+
+def create_train_resnet_cwt(pred_model, sample_size, window_size):
+    x1 = Input((sample_size, window_size, 3), name='x1')
+    x2 = Input((sample_size, window_size, 3), name='x2')
+    x3 = Input((sample_size, window_size, 3), name='x3')
+    y1_pred, y2_pred = pred_model([x1, x2, x3])
+    y1_true = Input(shape=(3,), name='y1_true')
+    y2_true = Input(shape=(4,), name='y2_true')
+    out = CustomMultiLossLayer(nb_outputs=2)([y1_true, y2_true, y1_pred, y2_pred])
+    train_model = Model([x1, x2, x3, y1_true, y2_true], out)
+    return train_model
